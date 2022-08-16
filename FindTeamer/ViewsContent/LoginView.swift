273a85503dec5dca71f7bfc71a6a48eba653ad6d
Nikita Sviridovich <ext-nikita.sviridovich@flowbird.group>
2022-08-16 10:27:00 +0200

    add firebase

---
+++ FindTeamer/ViewsContent/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  FindTeamer
+//
+//  Created by Nikita Sviridovich on 16.08.22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
