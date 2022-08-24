ff2c2f1eefea02973d315c423e447e3615c5222e
Nikita Sviridovich <Mikita.Svirydovich@itechart-group.com>
2022-08-24 11:02:40 +0200

    add main content view

---
+++ FindTeamer/ViewsContent/MainContentView.swift
@@ -0,0 +1,22 @@
+//
+//  MainContentView.swift
+//  FindTeamer
+//
+//  Created by Nikita Sviridovich on 23.08.22.
+//
+
+import SwiftUI
+
+struct MainContentView: View {
+    var body: some View {
+        List {
+            Text("TEST")
+        }.navigationBarBackButtonHidden(true)
+    }
+}
+
+struct MainContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainContentView()
+    }
+}
