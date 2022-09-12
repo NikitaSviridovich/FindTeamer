ae8d8c17e5bc42b75c50752fadf6d9d533ebe4d1
Nikita Sviridovich <Mikita.Svirydovich@itechart-group.com>
2022-09-12 13:22:13 +0200

    Update List & integrate autologin

---
+++ FindTeamer/ViewModels/RootViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  RootViewModel.swift
+//  FindTeamer
+//
+//  Created by Nikita Sviridovich on 12.09.22.
+//
+
+import SwiftUI
+import Combine
+import Firebase
+
+
+class RootViewModel: ObservableObject {
+    @Published var isAuthorized: Bool = false
+    var handle: AuthStateDidChangeListenerHandle?
+    public init() {
+        self.linten()
+    }
+    func linten() {
+        handle = Auth.auth().addStateDidChangeListener {(auth, user) in
+            if user != nil {
+                self.isAuthorized = true
+            } else {
+                self.isAuthorized = false
+            }
+        }
+        removeListener()
+    }
+    func removeListener() {
+        Auth.auth().removeStateDidChangeListener(handle!)
+    }
+    
+}
+
--- FindTeamer/ViewsContent/MainContent/MainComponents.swift
+++ FindTeamer/ViewsContent/MainContent/MainComponents.swift
@@ -13,21 +13,21 @@ struct eventCellGenerator : View  {
     
     var body: some View {
         NavigationLink(destination: EventDetailsView(event: event)) {
-                    Image(event.eventType.lowercased())
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 75, height: 75)
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text(event.eventType)
-                        .font(.system(size: 14, weight: .black, design: .rounded))
-                        .foregroundColor(.gray)
-                        Text(event.eventAddress)
-                            .font(.system(size: 12, weight: .heavy, design: .rounded))
-                        .foregroundColor(.gray)
-                        Text(event.eventTime.formatted())
-                        .font(.system(size: 10, weight: .medium, design: .rounded))
-                        .foregroundColor(Color.gray)
-                    }
+            Image(event.eventType.lowercased())
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 75, height: 75)
+            VStack(alignment: .leading, spacing: 4) {
+                Text(event.eventType)
+                    .font(.system(size: 14, weight: .black, design: .rounded))
+                    .foregroundColor(.gray)
+                Text(event.eventAddress)
+                    .font(.system(size: 12, weight: .heavy, design: .rounded))
+                    .foregroundColor(.gray)
+                Text(event.eventTime.formatted())
+                    .font(.system(size: 10, weight: .medium, design: .rounded))
+                    .foregroundColor(Color.gray)
+            }
         }
     }
 }
